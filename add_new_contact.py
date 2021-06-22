2201047a989c8f40fe987122726a655075b66cf8
suborov <v.suborov@gmail.com>
2021-06-22 23:42:36 +0700

    вынесла вспомогательные функции и setuo, teardown в отдельный класс = application. Преобразовала тест создания нового контакта с учетом нового класса

--- add_new_contact.py
+++ add_new_contact.py
@@ -1,125 +1,31 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 import unittest
 from contact import Contact
+from application import Applicatin
 
 class AddNewContact(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.app = Applicatin()
 
     def add_test_new_contact(self):
-        self.login("admin", "secret")
-        self.create_new_contact(Contact(firstname="Anna", middlename="Maria", lastname="Suborova", nickname="ASuboroa",
+        self.app.login("admin", "secret")
+        self.app.create_new_contact(Contact(firstname="Anna", middlename="Maria", lastname="Suborova", nickname="ASuboroa",
                                         title="friends", companyname="signatec", address="Novosibirsk",
                                         homephone="+79232274031", mobilephone="3834156789",
                                         mainemail="a.suborov_a@gmail.com", email2="anna_suborova@mail.ru", bday="28",
                                         bmonth="November", byear="1984"))
-        self.logout()
+        self.app.logout()
 
     def add_empty_test_new_contact(self):
-        self.login("admin", "secret")
-        self.create_new_contact(Contact(firstname="", middlename="", lastname="", nickname="", title="", companyname="",
+        self.app.login("admin", "secret")
+        self.app.create_new_contact(Contact(firstname="", middlename="", lastname="", nickname="", title="", companyname="",
                                         address="",
                                         homephone="", mobilephone="", mainemail="", email2="", bday="",
                                         bmonth="-", byear=""))
-        self.logout()
-
-    def logout(self):
-        wd = self.wd
-        # logout
-        wd.find_element_by_link_text("Logout").click()
-
-    def back_home_page(self):
-        wd = self.wd
-        # back home page
-        wd.find_element_by_link_text("home page").click()
-
-    def create_new_contact(self, contact):
-        wd = self.wd
-        # open page add new contact
-        wd.find_element_by_link_text("add new").click()
-        # init new contact
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.companyname)
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.homephone)
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobilephone)
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.mainemail)
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-        wd.find_element_by_name("theform").click()
-        # click create new contact
-        wd.find_element_by_xpath("//input[@value='Enter']").click()
-        #back nome page
-        self.back_home_page()
-
-    def login(self, loginname, password):
-        wd = self.wd
-        # open home page
-        self.open_home_page()
-        # login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(loginname)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        # home page
-        wd.get("http://localhost/addressbook/index.php")
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.wd.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
+        self.app.logout()
 
     def tearDown(self):
-        self.wd.quit()
+        self.app.district()
 
 if __name__ == "__main__":
     unittest.main()
---
+++ application.py
@@ -0,0 +1,106 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+
+class Applicatin:
+    def __init__(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    def logout(self):
+        wd = self.wd
+        # logout
+        wd.find_element_by_link_text("Logout").click()
+
+    def back_home_page(self):
+        wd = self.wd
+        # back home page
+        wd.find_element_by_link_text("home page").click()
+
+    def create_new_contact(self, contact):
+        wd = self.wd
+        # open page add new contact
+        wd.find_element_by_link_text("add new").click()
+        # init new contact
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.companyname)
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.homephone)
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobilephone)
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.mainemail)
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(contact.email2)
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact.byear)
+        wd.find_element_by_name("theform").click()
+        # click create new contact
+        wd.find_element_by_xpath("//input[@value='Enter']").click()
+        #back nome page
+        self.back_home_page()
+
+    def login(self, loginname, password):
+        wd = self.wd
+        # open home page
+        self.open_home_page()
+        # login
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(loginname)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self):
+        wd = self.wd
+        # home page
+        wd.get("http://localhost/addressbook/index.php")
+
+    def is_element_present(self, how, what):
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.wd.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+    def district(self):
+        self.wd.quit()
+
