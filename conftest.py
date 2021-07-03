d7e1698de74f82fedf0f9ce9f264e20e18c1af70
suborov <v.suborov@gmail.com>
2021-07-03 18:27:27 +0700

    валидация фикстуры перед каждым запуском

--- conftest.py
+++ conftest.py
@@ -1,12 +1,24 @@
 import pytest
 from fixture.application import Application
 
+fixture_create = None
 
-@pytest.fixture(scope="session")
-def app(request):
-    fixture_create = Application()
-    fixture_create.ses_h.login(loginname="admin", password="secret")
 
+@pytest.fixture
+def app():
+    global fixture_create
+    if fixture_create is None:
+        fixture_create = Application()
+        fixture_create.ses_h.login(loginname="admin", password="secret")
+    else:
+        if not fixture_create.is_valid():
+            fixture_create = Application()
+            fixture_create.ses_h.login(loginname="admin", password="secret")
+    return fixture_create
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
     def fin():
         fixture_create.ses_h.logout()
         Application.district(fixture_create)
