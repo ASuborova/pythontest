c33644a5ae012e5ddfaeccb77a0d99652e4e53dd
suborov <v.suborov@gmail.com>
2021-07-19 12:52:48 +0700

    в методы добавления новой группы и контакта добавлены проверки count

--- test/test_1_add_contact.py
+++ test/test_1_add_contact.py
@@ -9,8 +9,8 @@ def test_add_new_contact(app):
                 homephone="+79232274031", mobilephone="3834156789",
                 mainemail="a.suborov_a@gmail.com", email2="anna_suborova@mail.ru", bday="22", byear="1988")
     app.cont_h.create(contact)
+    assert len(old_list_contact) + 1 == app.cont_h.count()
     new_list_contact = app.cont_h.get_list_contact()
-    assert len(old_list_contact) + 1 == len(new_list_contact)
     old_list_contact.append(contact)
     assert sorted(old_list_contact, key=Contact.id_max) == sorted(new_list_contact, key=Contact.id_max)
 
--- test/test_4_add_group.py
+++ test/test_4_add_group.py
@@ -2,12 +2,11 @@ from model.group import Group
 
 
 def test_add_group(app):
-
     old_groups = app.gr.get_group_list()
     group = Group(namegroup="group_1", header="Zagolovok", footer="Podval")
     app.gr.create_group(group)
+    assert len(old_groups) + 1 == app.gr.count()
     new_groups = app.gr.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_max) == sorted(new_groups, key=Group.id_max)
 
