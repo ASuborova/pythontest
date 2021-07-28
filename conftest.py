5c8f2f3cfb2c5223ebbdcb251828dbe13974c20e
suborov <v.suborov@gmail.com>
2021-07-28 23:18:27 +0700

    загрузка информации о тесте из конфиг. файла, запуск конфиг.файла из командной строки, автоматическое определение пути к конфиг.файлу

--- conftest.py
+++ conftest.py
@@ -1,21 +1,26 @@
 import pytest
 from fixture.application import Application
+import json
+import os.path
 
 fixture_create = None
+target = None
 
 
 @pytest.fixture
 def app(request):
     global fixture_create
+    global target
     browser = request.config.getoption("--browser")
-    BUrl = request.config.getoption("--BUrl")
-    password = request.config.getoption("--password")
-    if fixture_create is None:
-        fixture_create = Application(browser=browser, BUrl=BUrl)
-    else:
-        if not fixture_create.is_valid():
-            fixture_create = Application(browser=browser, BUrl=BUrl)
-    fixture_create.ses_h.is_login(loginname="admin", password=password)
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    # BUrl = request.config.getoption("--BUrl")
+    # password = request.config.getoption("--password")
+    if fixture_create is None or not fixture_create.is_valid():
+        fixture_create = Application(browser=browser, BUrl=target['BUrl'])
+    fixture_create.ses_h.is_login(loginname=target['username'], password=target['password'])
     return fixture_create
 
 
@@ -31,5 +36,6 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--BUrl", action="store", default="http://localhost/addressbook/index.php")
-    parser.addoption("--password", action="store", default="secret")
+    # parser.addoption("--BUrl", action="store", default="http://localhost/addressbook/index.php")
+    # parser.addoption("--password", action="store", default="secret")
+    parser.addoption("--target", action="store", default="target.json")
