7b312c9a168e40fcd83fb1d881d99bbb99d58aa8
suborov <v.suborov@gmail.com>
2021-07-29 00:10:32 +0700

    повторила действия из видео 6_4

---
+++ data/add_group.py
@@ -0,0 +1,19 @@
+from model.group import Group
+import random
+import string
+
+constant = [
+    Group(namegroup="name1", header="header1", footer="footer1"),
+    Group(namegroup="name2", header="header2", footer="footer2")
+]
+
+
+def random_string(prefix, maxlen):
+    symbol = string.ascii_letters + string.digits # + string.punctuation + " "*3
+    return prefix + "".join(random.choice(symbol) for i in range(random.randrange(maxlen)))
+
+
+testdata = [Group(namegroup="", header="", footer="")] + [
+    Group(namegroup=random_string("group_name", 2), header=random_string("Zagolovok", 5), footer=random_string("Podval", 5))
+    for i in range(5)
+]
--- test/test_4_add_group.py
+++ test/test_4_add_group.py
@@ -1,25 +1,6 @@
 from model.group import Group
 import pytest
-import random
-import string
-
-
-def random_string(prefix, maxlen):
-    symbol = string.ascii_letters + string.digits # + string.punctuation + " "*3
-    return prefix + "".join(random.choice(symbol) for i in range(random.randrange(maxlen)))
-
-
-testdata = [Group(namegroup="", header="", footer="")] + [
-    Group(namegroup=random_string("group_name", 2), header=random_string("Zagolovok", 5), footer=random_string("Podval", 5))
-    for i in range(5)
-]
-
-# testdata = [
-#    Group(namegroup=group_name, header=Zagolovok, footer=Podval)
-#    for group_name in ["", random_string("group_name", 10)]
-#    for Zagolovok in ["", random_string("Zagolovok", 20)]
-#    for Podval in ["", random_string("Podval", 20)]
-#]
+from data.add_group import constant as testdata
 
 
 @pytest.mark.parametrize("group", testdata, ids=(repr(x) for x in testdata))
@@ -31,15 +12,6 @@ def test_add_group(app, group):
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_max) == sorted(new_groups, key=Group.id_max)
 
-
-# def test_add_empty_group(app):
-#    old_groups = app.gr.get_group_list()
-#    group = Group(namegroup="", header="", footer="")
-#    app.gr.create_group(group)
-#    new_groups = app.gr.get_group_list()
-#    assert len(old_groups) + 1 == len(new_groups)
-#    old_groups.append(group)
-#    assert sorted(old_groups, key=Group.id_max) == sorted(new_groups, key=Group.id_max)
 
 
 
